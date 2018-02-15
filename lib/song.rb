e623423b54274a3bbccfb99d19120c7ba33ce54d
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-15 20:29:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
   def self.all
     @@all
   end
@@ -10,4 +16,14 @@ class Song
     self.class.all << self
   end
 
+  def self.new_by_name(string)
+    song.name = string
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @all << song
+    song.name
+  end
 end
